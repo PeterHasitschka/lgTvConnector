5adbe1de89e5909bdbb17fd52d92fe8208805100
Peter Hasitschka <peter.hasitschka@probots.io>
2021-11-19 23:15:44 +0100

    Renaming project

--- ios/LGTVConnectorTests/LGTVConnectorTests.m
+++ ios/LGTVConnectorTests/LGTVConnectorTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MyNativeProjectTests : XCTestCase
+@interface LGTVConnectorTests : XCTestCase
 
 @end
 
-@implementation MyNativeProjectTests
+@implementation LGTVConnectorTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
